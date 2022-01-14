23a1416485abc34e15871793b987e8f2acc63a3b
Rafael Oliveira <rafdev.x@gmail.com>
2022-01-14 19:25:08 +0000

    fix threads_truncate_and_append

--- tests/threads_truncate_and_append.c
+++ tests/threads_truncate_and_append.c
@@ -4,11 +4,12 @@
 #include <string.h>
 
 #define LEN 1555
+#define THREADS 19
 
 void *truncate(void *ipath) {
 
     char str[LEN];
-    memset(str, 'A', LEN);
+    memset(str, 'C', LEN);
     char *path = (char *)ipath;
 
     int f;
@@ -28,7 +29,7 @@ void *truncate(void *ipath) {
 void *append(void *ipath) {
 
     char str[LEN];
-    memset(str, 'A', LEN);
+    memset(str, 'B', LEN);
     char *path = (char *)ipath;
 
     int f;
@@ -48,44 +49,58 @@ void *append(void *ipath) {
 void write_to_file(char *path, char *contents) {
     int f = tfs_open(path, TFS_O_CREAT);
     assert(f != -1);
-    ssize_t r = tfs_write(f, contents, strlen(contents));
-    assert(r == strlen(contents));
+    ssize_t r = tfs_write(f, contents, LEN);
+    assert(r == LEN);
     assert(tfs_close(f) != -1);
 }
 
 int main() {
-    char *path_append = "/append.txt";
-    char *path_trunc = "/trunc.txt";
-    char str[LEN];
-    memset(str, 'A', LEN);
+    char path[THREADS][10];
+    char strA[LEN];
+    memset(strA, 'A', LEN);
+    char strB[LEN];
+    memset(strB, 'B', LEN);
+    char strC[LEN];
+    memset(strC, 'C', LEN);
     assert(tfs_init() != -1);
-
-    write_to_file(path_append, str);
-    write_to_file(path_trunc, str);
-
-    pthread_t tid[19];
-    for (int i = 0; i < 19; i++) {
-        if (i % 2 == 0)
-            pthread_create(&tid[i], NULL, &append, (void *)path_append);
-        else
-            pthread_create(&tid[i], NULL, &truncate, (void *)path_trunc);
+    for (int i = 0; i < THREADS; i++) {
+        sprintf(path[i], "/%d.txt", i);
+        write_to_file(path[i], strA);
     }
-    for (int i = 0; i < 19; i++) {
+    pthread_t tid[THREADS];
+    for (int i = 0; i < THREADS; i++) {
+        if (i % 2 == 0) {
+            pthread_create(&tid[i], NULL, &append, (void *)path[i]);
+        } else {
+            pthread_create(&tid[i], NULL, &truncate, (void *)path[i]);
+        }
+    }
+
+    for (int i = 0; i < THREADS; i++) {
         pthread_join(tid[i], NULL);
+
+        int f = tfs_open(path[i], 0);
+        assert(f != -1);
+
+        char tmp[LEN];
+        if (i % 2 == 0) {
+            // append
+            assert(tfs_read(f, tmp, LEN) == LEN);
+            assert(memcmp(strA, tmp, LEN) == 0);
+            assert(tfs_read(f, tmp, LEN) == LEN);
+            assert(memcmp(strB, tmp, LEN) == 0);
+            assert(tfs_read(f, tmp, LEN) == 0);
+        } else {
+            // truncate
+            assert(tfs_read(f, tmp, LEN) == LEN);
+            assert(memcmp(strC, tmp, LEN) == 0);
+
+            assert(tfs_read(f, tmp, LEN) == 0);
+        }
+        assert(tfs_close(f) == 0);
     }
 
-    // TODO: check
-
-    /*f = tfs_open(path, 0);
-    assert(f != -1);
-
-    r = tfs_read(f, buffer, sizeof(buffer) - 1);
-    assert(r == strlen(str));
-
-    buffer[r] = '\0';
-    assert(strcmp(buffer, str) == 0);
-
-    assert(tfs_close(f) != -1);*/
+    assert(tfs_destroy() == 0);
 
     printf("Successful test.\n");
     return 0;

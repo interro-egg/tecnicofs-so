69a95e9503bd00d2b2f6ef871e96bdcde4b93dc0
Rafael Oliveira <rafdev.x@gmail.com>
2022-02-02 15:11:03 +0000

    extract read_server_pipe

--- fs/server_parsers.c
+++ fs/server_parsers.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 
 int parse_tfs_mount(int server_pipe_fd, tfs_session_data_t *data) {
-  // FIXME: vv is this if-read operation worth abstracting?
-  if (read(server_pipe_fd, data->client_pipe_path,
-           MAX_PIPE_NAME * sizeof(char)) == -1) {
+  read_server_pipe(server_pipe_fd, data->client_pipe_path,
+                   MAX_PIPE_NAME * sizeof(char));
+  return 0;
+}
+
+void read_server_pipe(int server_pipe_fd, void *buf, size_t n_bytes) {
+  if (read(server_pipe_fd, buf, n_bytes) == -1) {
     fprintf(stderr, "[ERR]: read failed: %s\n", strerror(errno));
     exit(EXIT_FAILURE);
   } // TODO: read returns 0 if pipe closed (maybe close & re-open?)
-
-  return 0;
 }
--- fs/server_parsers.h
+++ fs/server_parsers.h
@@ -11,4 +11,6 @@ int parse_tfs_read(int server_pipe_fd, tfs_session_data_t *data);
 int parse_tfs_shutdown_after_all_closed(int server_pipe_fd,
                                         tfs_session_data_t *data);
 
+void read_server_pipe(int server_pipe_fd, void *buf, size_t n_bytes);
+
 #endif /* SERVER_PARSERS_H */
